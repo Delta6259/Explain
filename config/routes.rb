51c2854a2aa41099570391fc1e28aeb75f82c86d
guillaume trupin <gtrupin@gmail.com>
2017-09-08 15:06:59 +0200

    profile ok et liens dans la navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/profile', to: 'profiles#profile'
   resources :articles
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
