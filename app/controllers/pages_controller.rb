594f1ed94d88e18ddbdbb21f9bbfca93abd1b820
Guillaume Trupin <gtrupin@gmail.com>
2017-09-19 10:09:29 +0200

    top card home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @article_1 = Article.all.order('vote DESC').first
+    @article_2 = Article.all.order('vote DESC').second
+    @article_3 = Article.all.order('vote DESC').third
+    @article_4 = Article.all.order('vote DESC').fourth
   end
 end
