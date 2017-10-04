e2dc7280f1d96c151ed157e78830072c6da839cf
Guillaume Trupin <gtrupin@gmail.com>
2017-10-04 08:35:59 +0200

    cancel

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   mount_uploader :photo, PhotoUploader
   # validates :category, presence: false
 end
