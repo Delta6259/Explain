0fe8220ea356d98a50f3e2bc503ce054350cc930
Guillaume Trupin <gtrupin@gmail.com>
2017-10-11 13:06:26 +0200

    api meteo

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,7 +15,12 @@ class ProfilesController < ApplicationController
     url = "https://api.darksky.net/forecast/6919bc2ae7b083bdac732a36b67983b1/#{@latitude},#{@longitude}" # L'url retourne un JSON à scraper qui contient les informations essentielles.
 
     html_file = open(url).read
-    html_doc = Nokogiri::HTML(html_file)
+    meteo = JSON.parse(html_file)
+    @weather_now_description = meteo['currently']['summary']
+    temperature_in_f = meteo['currently']['temperature']
+    @temperature_in_c = (temperature_in_f - 32) * 5/9
+    @temperature_in_c.ceil
+    # html_doc = Nokogiri::HTML(html_file)
     # html_doc.search('.k').each do |element|
     #   @content = element.text
     #    # puts element.attribute('href')
